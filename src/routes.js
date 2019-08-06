d5c044bc9e4c50b8eca7b4fd1cdca6acab010fda
Matheus Ricardo dos Santos <matheus.r.d.s10@hotmail.com>
2019-08-06 20:53:07 -0300

    Implement list of avaliable devs

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const DislikeController = require('./controllers/DislikeController');
 const routes = express.Router();
 
 routes.post('/devs', DevController.store);
+routes.get('/devs', DevController.index);
 routes.post('/devs/:devId/likes', LikeController.store);
 routes.post('/devs/:devId/dislikes', DislikeController.store);
-
 module.exports = routes;

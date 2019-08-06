60418a39e17789edca64c7c073126ae9ad17b168
Matheus Ricardo dos Santos <matheus.r.d.s10@hotmail.com>
2019-08-06 20:55:51 -0300

    Added cors as dependency

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 const server = express();
 
 mongoose.connect('mongodb+srv://user1:user1@tindev-a6ocv.mongodb.net/Tindev?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+server.use(cors());
 server.use(express.json())
 server.use(routes);
 server.listen(3333);
